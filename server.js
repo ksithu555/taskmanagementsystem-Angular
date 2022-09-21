ee4d8e8a8b55cae24299f96e7546b3774b8818da
@ksithu555 <ksithu555@gmail.com>
2022-09-21 19:31:22 +0630

    Update

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/taskmanagementsystem-Angular'));
+app.use(express.static(__dirname + '/dist/task-manager'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/taskmanagementsystem-Angular/index.html'));
+res.sendFile(path.join(__dirname+'/dist/task-manager/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
